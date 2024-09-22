63dfe08bcbc3c49c0a9380ae9363a6dbdb936ca1
Ns-AnoNymouS <lankotunaveen@gmail.com>
2024-09-22 11:12:27 +0530

    fixed conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import ForgotPasswordForm from './components/ForgotPassword';
 
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 
