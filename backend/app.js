26c8a133033a472ad59becadd20bccbcc9725bfb
lahariSane <135339666+lahariSane@users.noreply.github.com>
2024-09-25 11:12:32 +0530

    errors taken care

--- backend/app.js
+++ backend/app.js
@@ -23,19 +23,8 @@ app.use(express.json());
 app.use('/', userRouter)
 app.use('/', adminRouter) 
 app.use('/api', mailRouter)
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Connected to MongoDB via Mongoose");
-    } catch (err) {
-        console.error('Failed to connect to MongoDB', err);
-        process.exit();
-    }
-};
-connectDB();
+
+db.connect();
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
