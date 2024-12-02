44b098f5d13ea0685710a116b69efc6e699073ea
Tanush Reddy <atanushreddy@gmail.com>
2024-12-03 01:22:06 +0530

    .

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import Profile from '../components/Profile'
-import "Settings.css"
+import "../styles/Settings.css";
 
 export default function Settings() {
     return (
