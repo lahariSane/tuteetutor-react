018f4f4227209eed9f2cff5197bacb509571991a
Tanush Reddy <atanushreddy@gmail.com>
2024-12-03 13:10:27 +0530

    rectified all the errors

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -15,8 +15,8 @@ export default function Profile() {
                                 <img className='profile-picture' src="https://via.placeholder.com/100" alt="Profile Photo" />
                             </div>
                             <div style={{ position: 'relative', top: "-3em" }}>
-                                <h2 style={{ marginBottom: '0.3em' }}>Your Photo</h2>
-                                <p>This will be displayed on your profile</p>
+                                <h2 style={{ marginBottom: '0.3em', fontSize: '20px', margin:0 }}>Your Photo</h2>
+                                <p style={{color: '#555', fontSize: '14px', margin: '5px 0 20px'}}>This will be displayed on your profile</p> 
                             </div>
                             <div className="buttons">
                                 <button className="upload-button">Upload New</button>
@@ -28,14 +28,14 @@ export default function Profile() {
                 <div className='box-2'>
                     <div className="personal-info">
                         <div style={{ borderBottom: '2px solid #007bff' }}>
-                            <h2 style={{ marginBottom: '0.3em' }} > <b> Personal Information</b></h2>
+                            <h2 style={{ marginBottom: '0.3em', fontSize: '20px', margin:0,   marginBottom: '20px', color: '#333'}} > <b> Personal Information</b></h2>
                         </div>
                         <form className="personal-info-form" style={{ marginTop: '1em' }}>
                             <div className="form-group">
                                 <label htmlFor="fullName"><b>Full Name</b></label>
                                 <div className="input-wrapper">
                                     <span className="icon">👤</span>
-                                    <input type="text" id="fullName" placeholder="Full Name" />
+                                    <input className='profile-input' type="text" id="fullName" placeholder="Full Name" />
                                 </div>
                             </div>
 
@@ -43,14 +43,14 @@ export default function Profile() {
                                 <label htmlFor="email"> <b> Email address</b></label>
                                 <div className="input-wrapper">
                                     <span className="icon">📧</span>
-                                    <input type="email" id="email" placeholder="Email" />
+                                    <input className='profile-input' type="email" id="email" placeholder="Email" />
                                 </div>
                             </div>
 
                             <div className="form-group">
                                 <label htmlFor="role"><b>Role</b></label>
                                 <div className="input-wrapper">
-                                    <input type="text" id="role" placeholder="Role" />
+                                    <input className='profile-input' type="text" id="role" placeholder="Role" />
                                 </div>
                             </div>
                         </form>
@@ -61,7 +61,7 @@ export default function Profile() {
                 <div className='box-3'>
                     <div className="bio">
                         <div style={{ borderBottom: '2px solid white' }}>
-                            <h2 style={{ marginBottom: '0.3em', color: 'white' }} > <b> Bio</b></h2>
+                            <h2 style={{ marginBottom: '0.3em', color: 'white' , fontSize: '20px', margin:0}} > <b> Bio</b></h2>
                         </div>
                         <div className="bio-content">
                             <textarea width="40em" id="bio" placeholder="Write something about yourself..." rows="4" cols="50" minRows="4"></textarea>
@@ -69,7 +69,7 @@ export default function Profile() {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
 
 
 
